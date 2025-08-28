572d7ef67ce2c689d9093962b1f081b2d9433b8f
Sujan <sujanchakma679@gmail.com>
2025-08-28 23:17:14 +0900

    heart icon fuctionality added

--- heart.js
+++ heart.js
@@ -1,1 +1,24 @@
-console.log('heart js button connected')
+
+
+// document.querySelectorAll('.heart-btn')
+// .addEventListener('click', function(event){
+//     event.preventDefault();
+
+//    console.log('heart button clicke')
+// })
+
+document.querySelectorAll('.heart-btn').forEach(button => {
+  button.addEventListener('click', function(event) {
+    event.preventDefault();
+    // console.log('heart button clicked');
+
+    const heart = parseInt(document.getElementById('heart').innerText)
+
+    // console.log(heart)
+    const heartCount = heart + 1;
+    document.getElementById('heart').innerText = heartCount;
+
+
+  });
+});
+
